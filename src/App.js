4ddae808e9ee513f4efecc5f1dfc2f264ec5c862
Amir Dhahri <amirdhahri01@gmail.com>
2024-11-21 22:28:27 +0100

    An update

--- src/App.js
+++ src/App.js
@@ -2,38 +2,61 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [formData , setFormData] = useState({
-    name:"",
+  const [formData, setFormData] = useState({
+    name : "",
     age : 18,
-    country:"Tunisie",
-    generalInfo:"",
-    isStudent:false
+    country : "Tunisie",
+    generalInfo : "",
+    programmingLanguage : "",
+    isStudent : false,
+    status : ""
   });
   const handleChange = (e) => {
     setFormData(oldFormData => {
       return {
         ...oldFormData,
-        [e.target.name] : e.target.value
+        [e.target.name]: e.target.value
       }
     })
-    console.log(formData);
-    
   }
+  const handleCheckbox = (e) => {
+    setFormData(oldFormData => {
+      return {
+        ...oldFormData,
+        [e.target.name]: e.target.checked
+      }
+    })
+  }
+  console.log(formData);
   return (
     <>
       <form action="">
         <label htmlFor="name">Name : </label>
-        <input value={formData.name} name="name" type="text" id="name" onChange={handleChange}/>
+        <input value={formData.name} name="name" type="text" id="name" onChange={handleChange} />
         <label htmlFor="age">Age : </label>
-        <input value={formData.age} name="age" type="text" id="age" onChange={handleChange}/>
+        <input value={formData.age} name="age" type="text" id="age" onChange={handleChange} />
         <label htmlFor="country">Country : </label>
-        <input value={formData.country} name="country" type="text" id="country" onChange={handleChange}/>
+        <input value={formData.country} name="country" type="text" id="country" onChange={handleChange} />
         <label htmlFor="student">Are you a student</label>
-        <input type="checkbox" id="student" />
+        <input type="checkbox" id="student" name="isStudent" checked={formData.isStudent} onChange={handleCheckbox} />
         <label htmlFor="generalInfo">General Info : </label>
         <textarea name="" id="generalInfo" onChange={handleChange}>
           {formData.generalInfo}
         </textarea>
+        <hr />
+        <select name="programmingLanguage" value={formData.programmingLanguage} onChange={handleChange}>
+          <option value="java">Java</option>
+          <option value="python">Python</option>
+          <option value="ruby">Ruby</option>
+          <option value="php">PHP</option>
+        </select>
+        <hr />
+        <div>
+          <input value="student" type="radio" name="status" id="status" checked={formData.status === "student"} onChange={handleChange}/>
+          Student
+          <input value="teacher" type="radio" name="status" id="status" checked={formData.status === "teacher"} onChange={handleChange}/>
+          Teacher
+        </div>
       </form>
     </>
   );
