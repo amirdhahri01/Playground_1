8f43aa7d29d03da32e9034febf15fdf37f772c2c
Amir Dhahri <amirdhahri01@gmail.com>
2024-11-22 15:25:18 +0100

    An uodate

--- src/hooks/context/Context.jsx
+++ src/hooks/context/Context.jsx
@@ -3,14 +3,14 @@ import User from './User';
 
 export const UserContext = createContext(null);
 
-
 function Context() {
+  
   const [userOne, setUserOne] = useState("Amir Dhahri");
 
   return (
-    <div>
-      <User user={userOne} />
-    </div>
+    <UserContext.Provider value={userOne}>
+      <User/>
+    </UserContext.Provider>
   )
 }
 
